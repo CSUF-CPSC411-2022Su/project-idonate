5757eca431269f11b68b8e8e64dbbd756925525b
Daniel Yoshida <dtyoshid@csu.fullerton.edu>
2022-06-08 11:21:40 -0700

    Initial Commit

---
+++ iDonate/iDonateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDonateApp.swift
+//  iDonate
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iDonateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
