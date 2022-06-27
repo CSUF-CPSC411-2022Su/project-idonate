4c5ecf9d339a00da4708bbde3ddc53036bb7043d
suhys <yuseung.suh@gmail.com>
2022-06-27 11:20:30 -0700

    Create Donation_InputDonate.swift

--- iDonate/ContentView.swift
+++ iDonate/ContentView.swift
@@ -8,68 +8,12 @@
 import SwiftUI
 import Combine
 
-var foods: [FoodData] = foodList.list
-var locations: [Data] = LocationList.list
-
 
 struct ContentView: View {
-    @ObservedObject var donationList = DonationList()
-    @State var item: String = ""
-    @State var itemAddMessage: String = ""
-    @State var list = [String]()
-//    var foods: FoodData
-    
-
-    func addNewItem() {
-        if item != "" {
-//            if foods.foodName.contains(item) {
-                    donationList.donations.append(Donation(DonationItem: item))
-                    self.item = ""
-//            }
-        }
-    }
-    
     var body: some View {
-        NavigationView{
-            VStack{
-                VStack{
-                    HStack {
-                        VStack{
-                            Text("Input the food you want to donate")
-                            HStack{
-                                TextField("Food", text: $item)
-                                Button(action: self.addNewItem, label: {
-                                    Text("Add")
-                            }
-                                )}
-                        }
-                    }.padding()
-                    EditButton()
-                    List {
-                        ForEach(donationList.donations) {
-                        donation in
-                        Text(donation.DonationItem)
-                            .font(.subheadline)
-                    }.onDelete {
-                        offset in
-                        donationList.donations.remove(atOffsets: offset)
-                                   }
-                    }
-                }
-                .padding()
-
-                VStack{
-                    NavigationLink(destination: LocationsView()) {
-                        Text("Locations")
-                    }
-                    }
-                Spacer()
-                .navigationTitle(Text("Donation"))
-            }
-        }
-        
+        AddDonation()
     }
-    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
